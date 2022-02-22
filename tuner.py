f0ea473938500f8ddd6b4a908be591b59406339a
mbridak <michael.bridak@gmail.com>
2022-02-21 20:42:13 -0800

    PEP8 and refactoring.

--- tuner.py
+++ tuner.py
@@ -1,38 +1,47 @@
 #!/usr/bin/python3
-
-import logging
-logging.basicConfig(level=logging.WARNING)
-
+"""Changed your radio to a 'safe' frequency to tune it."""
+# pylint: disable=broad-except
 import xmlrpc.client
-import os, sys
-from typing import final
+import os
+import sys
+import logging
+
+
 from PyQt5 import QtCore, QtWidgets, uic
 from PyQt5.QtCore import QDir
 from PyQt5.QtGui import QFontDatabase
 
+logging.basicConfig(level=logging.WARNING)
+
+
 def relpath(filename):
-		try:
-			base_path = sys._MEIPASS # pylint: disable=no-member
-		except:
-			base_path = os.path.abspath(".")
-		return os.path.join(base_path, filename)
+    """
+    Checks to see if program has been packaged with pyinstaller.
+    If so base dir is in a temp folder.
+    """
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+        base_path = getattr(sys, "_MEIPASS")
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, filename)
+
 
 def load_fonts_from_dir(directory):
-		families = set()
-		for fi in QDir(directory).entryInfoList(["*.ttf", "*.woff", "*.woff2"]):
-			_id = QFontDatabase.addApplicationFont(fi.absoluteFilePath())
-			families |= set(QFontDatabase.applicationFontFamilies(_id))
-		return families
+    """Load font families"""
+    families_set = set()
+    for thing in QDir(directory).entryInfoList(["*.ttf", "*.woff", "*.woff2"]):
+        _id = QFontDatabase.addApplicationFont(thing.absoluteFilePath())
+        families_set |= set(QFontDatabase.applicationFontFamilies(_id))
+    return families_set
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    oldfreq = ''
-    oldmode = ''
-    oldbw = ''
-    settings_dict ={
-        "host": "127.0.0.1",
-        "port": 4532
-    }
+    """the main window"""
+
+    oldfreq = ""
+    oldmode = ""
+    oldbw = ""
+    settings_dict = {"host": "127.0.0.1", "port": 4532}
     ft8freq = {
         "2": "7074000",
         "6": "50323000",
@@ -44,51 +53,57 @@ class MainWindow(QtWidgets.QMainWindow):
         "30": "10136000",
         "40": "7074000",
         "80": "3573000",
-        "160": "1840000"
+        "160": "1840000",
     }
 
     def __init__(self, *args, **kwargs):
+        """Initialize"""
         super().__init__(*args, **kwargs)
         uic.loadUi(self.relpath("main.ui"), self)
         self.tunebutton.clicked.connect(self.mainloop)
-        self.oldfreq, self.oldmode, self.oldbw = self.getCurrentRadioState()
+        self.oldfreq, self.oldmode, self.oldbw = self.get_current_radio_state()
         self.freq_label.setText(self.oldfreq)
         self.mode_label.setText(self.oldmode)
         self.server = xmlrpc.client.ServerProxy("http://localhost:12345")
 
-    def relpath(self, filename):
+    @staticmethod
+    def relpath(filename: str) -> str:
         """
-        lets the program know where the temp execution location is.
+        If the program is packaged with pyinstaller,
+        this is needed since all files will be in a temp
+        folder during execution.
         """
-        try:
-            base_path = sys._MEIPASS # pylint: disable=no-member
-        except:
+        if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+            base_path = getattr(sys, "_MEIPASS")
+        else:
             base_path = os.path.abspath(".")
-        finally:
-            return os.path.join(base_path, filename)
+        return os.path.join(base_path, filename)
 
-    def getCurrentRadioState(self):
+    def get_current_radio_state(self):
+        """Gets the current radio state. Surprise!"""
         try:
             freq = self.server.rig.get_vfo()
             mode = self.server.rig.get_mode()
-            bw = self.server.rig.get_bw()[0]
+            bandwidth = self.server.rig.get_bw()[0]
             self.errorline_label.setText("")
-            logging.info(f"getCurrentRadioState:{freq} {mode} {bw}")
-            return freq, mode, bw
-        except Exception as e:
-            self.errorline_label.setText(f"{e}")
+            logging.info("get_current_radio_state:%s %s %s", freq, mode, bandwidth)
+            return freq, mode, bandwidth
+        except Exception as err:
+            self.errorline_label.setText(f"{err}")
             return "0000000", "ERR", "0"
 
     def changefreq(self, tunefreq):
-        logging.info(f"changefreq: {tunefreq}")
+        """Changes the radio frequency"""
+        logging.info("changefreq: %s", tunefreq)
         try:
             self.server.rig.set_frequency(float(tunefreq))
-        except Exception as e:
-            self.errorline_label.setText(f"{e}")
+        except Exception as err:
+            self.errorline_label.setText(f"{err}")
             return
 
     def changemode(self, isft8):
-        logging.info(f"changemode:{isft8}")
+        """Changes the radios mode"""
+        logging.info("changemode:%s", isft8)
         try:
             if isft8:
                 self.server.rig.set_mode("CW")
@@ -96,8 +111,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 return
             self.server.rig.set_mode(self.oldmode)
             self.server.rig.set_bandwidth(int(self.oldbw))
-        except Exception as e:
-            self.errorline_label.setText(f"{e}")
+        except Exception as err:
+            self.errorline_label.setText(f"{err}")
             return
 
     def getband(self, freq):
@@ -108,79 +123,85 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         if freq.isnumeric():
             frequency = int(float(freq))
-            if frequency > 1800000 and frequency < 2000000:
+            if 2000000 > frequency > 1800000:
                 return "160"
-            if frequency > 3500000 and frequency < 4000000:
+            if 4000000 > frequency > 3500000:
                 return "80"
-            if frequency > 5330000 and frequency < 5406000:
+            if 5406000 > frequency > 5330000:
                 return "60"
-            if frequency > 7000000 and frequency < 7300000:
+            if 7300000 > frequency > 7000000:
                 return "40"
-            if frequency > 10100000 and frequency < 10150000:
+            if 10150000 > frequency > 10100000:
                 return "30"
-            if frequency > 14000000 and frequency < 14350000:
+            if 14350000 > frequency > 14000000:
                 return "20"
-            if frequency > 18068000 and frequency < 18168000:
+            if 18168000 > frequency > 18068000:
                 return "17"
-            if frequency > 21000000 and frequency < 21450000:
+            if 21450000 > frequency > 21000000:
                 return "15"
-            if frequency > 24890000 and frequency < 24990000:
+            if 24990000 > frequency > 24890000:
                 return "12"
-            if frequency > 28000000 and frequency < 29700000:
+            if 29700000 > frequency > 28000000:
                 return "10"
-            if frequency > 50000000 and frequency < 54000000:
+            if 54000000 > frequency > 50000000:
                 return "6"
-            if frequency > 144000000 and frequency < 148000000:
+            if 148000000 > frequency > 144000000:
                 return "2"
         else:
             return "0"
 
-    def pollIdleRadio(self):
+    def poll_idle_radio(self):
+        """Polls the radio"""
         if self.tunebutton.isChecked():
             return
-        freq, mode, _ = self.getCurrentRadioState()
+        freq, mode, _ = self.get_current_radio_state()
         self.freq_label.setText(freq)
         self.mode_label.setText(mode)
 
     def tune(self):
-        self.oldfreq, self.oldmode, self.oldbw = self.getCurrentRadioState()
+        """Set radio up for tune."""
+        self.oldfreq, self.oldmode, self.oldbw = self.get_current_radio_state()
         band = self.getband(self.oldfreq)
         if band:
             self.changefreq(self.ft8freq[band])
             self.changemode(True)
-            freq, mode, _ = self.getCurrentRadioState()
+            freq, mode, _ = self.get_current_radio_state()
             self.freq_label.setText(freq)
             self.mode_label.setText(mode)
         info = self.server.rig.get_info()
         if "R:IC-7300" in info:
             self.server.rig.cat_string("xFExFEx94xE0x1Cx01x02xFD")
 
-
-    def doneTuning(self):
+    def done_tuning(self):
+        """Change the radio state back after tune"""
         self.changefreq(self.oldfreq)
         self.changemode(False)
-        freq, mode, _ = self.getCurrentRadioState()
+        freq, mode, _ = self.get_current_radio_state()
         self.freq_label.setText(freq)
         self.mode_label.setText(mode)
 
     def mainloop(self):
+        """Handles the state"""
         if self.tunebutton.isChecked():
             self.tune()
             return
-        self.doneTuning()
+        self.done_tuning()
+
 
 def main():
+    """The main loop"""
     app = QtWidgets.QApplication(sys.argv)
-    app.setStyle('Fusion')
+    app.setStyle("Fusion")
     font_dir = relpath("font")
     families = load_fonts_from_dir(os.fspath(font_dir))
     logging.info(families)
     window = MainWindow()
     window.show()
     timer = QtCore.QTimer()
-    timer.timeout.connect(window.pollIdleRadio)
+    timer.timeout.connect(window.poll_idle_radio)
     timer.start(200)
     app.exec()
+
 
 if __name__ == "__main__":
     main()
